8ab08233756b80f162df2092fdaf2d7772e08437
rickyscoder <rickyzhz@foxmail.com>
2018-03-18 20:39:23 +0800

    添加mongo 操作方法

---
+++ db/classes/dbclass.js
@@ -0,0 +1,67 @@
+/**
+ * created by ricky 2018-3-18 20:33:45
+ */
+
+/**
+ * @private instance
+ */
+
+var dbOpreation = null 
+
+/**
+ * @public DBopreation
+ */
+
+class DBopreation {
+    
+    constructor( option ){
+        option
+            ? null
+            : option = {}
+        this.connect_url = option.url
+    }
+
+    /**
+     * 获取集合
+     * @param {*} collection 集合名称
+     */
+
+    getCollection( db, collection ){
+        return db.collection( collection )
+    }
+    
+    /**
+     * 插入数据
+     * @param {*} db 
+     * @param {*} data 
+     * @param {*} callback 
+     */
+
+    insertData(db, data, callback){
+        if( !db || !data || typeof callback !== 'function' ){
+            console.log( ...arguments ,'---参数有误----')
+            return false
+        }
+        let cllection = this.getCollection( db, 'products' )
+        collection.insert(data, function(err, res){
+            if( err ) {
+                console.log(err)
+                return
+            }
+            callback(res)           
+        })
+    }
+}
+
+if( !dbOpreation ){
+    dbOpreation = new MyprojectDB()
+}
+
+module.exports = {
+    DBopreation: dbOpreation
+}
+// const interface = {
+//     url: "test url"
+// }
+// var subdb = new MyprojectDB( interface )
+// subdb.insertData()
--- db/db.js
+++ db/db.js
@@ -1,38 +1,50 @@
-var mongo = require('mongodb').MongoClient //引入mongo客户端对象
-var DB_CONN_STR = 'mongodb://localhost:12345/myproject'  //创建数据库连接
+const mongo = require('mongodb').MongoClient //引入mongo客户端对象
+const DB_CONN_STR = 'mongodb://localhost:12345/myproject'  //创建数据库连接
+const dnop = require('./classes/dbclass.js').DBopreation
+//使用客户端连接数据，并指定完成时的回调方法
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+    console.log("连接成功！");
+    const data = [{_id:7,"name":'rose',"age":21},{_id:8,"name":'mark',"age":22}]
+    //执行插入数据操作，调用自定义方法
+    dnop.insertData(db, data, function(result) {
+        //显示结果
+        console.log(result);
+        //关闭数据库
+        db.close();
+    });
+});
 
-const interface = {
-    url: DB_CONN_STR
-}
-/**
- * @private instance
- */
-var instance = null
 
-/**
- * @public MyprojectDB
- */
-class MyprojectDB {
-    
-    constructor( option ){
-        option
-            ? null
-            : option = {}
-        this.connect_url = option.url
-    }
 
-    insertData(){
-        console.log('-------this is insert function----------')
-        console.log(`--------this.connect_url----------`)
-    }
-}
 
-if( !instance ){
-    instance = new MyprojectDB()
-}
 
-module.exports = {
-    MyprojectDB: instance
-}
-// var subdb = new MyprojectDB( interface )
-// subdb.insertData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
