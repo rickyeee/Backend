f1b6bdee724c1dadd9e4f2674f4b76bd9c4ff982
rickyscoder <rickyzhz@foxmail.com>
2018-03-28 01:05:45 +0800

    get api test

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var test = require('./routes/api/test');
 
 var app = express();
+
+app.disable('x-powered-by')//?????
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api/test', test);
 
-// catch 404 and forward to error handler
+// if non-match any router-path, catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log('meet an error')
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -35,9 +40,11 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+//   console.log(err.stack)
+  res.type('text/html');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
