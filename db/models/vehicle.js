5ffacacdb591f84ccf53c1ef65b13e6cd9be4dbe
rickyscoder <rickyzhz@foxmail.com>
2018-04-06 00:55:32 +0800

    extract model file

---
+++ db/models/vehicle.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const vehicleSchema = new mongoose.Schema({
+    name: String,
+    id: Number,
+    type: String
+})
+vehicleSchema.methods.getName = function () {
+    console.log(this.name)
+}
+console.log('第一次引用')//测试多次引用是否会缓存
+module.exports = mongoose.model('vehicle', vehicleSchema);
