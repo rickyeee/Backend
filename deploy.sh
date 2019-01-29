da36510ff7786fdd938ba3201ff8d4040990e9ce
张瑞琦 <zhangruiqi@corp.rrzcp8.com>
2019-01-29 15:02:21 +0800

    [修改sh调用规则]

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-rm -rf /usr/local/nginx/html/resume/*
-cp -r ./public/* /usr/local/nginx/html/resume/
+# 不要随意修改这个脚本
+# $1 [PROJECT_NAME] 项目名称
+if [ $# -ne 1 ];then
+  echo "Usage : $0 [PROJECT_NAME]" >&2
+  exit 1
+fi
+# 清空目录
+# nginx目录 /usr/local/nginx/html/[PROJECT_NAME]/
+rm -rf /usr/local/nginx/html/$1/*
+# 拷贝静态资源
+cp -r ./public/* /usr/local/nginx/html/$1/
+# 启动项目
 npm run start
