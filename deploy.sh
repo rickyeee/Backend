bf7beba086d834926d2ecb9020abca2c36350254
张瑞琦 <zhangruiqi@corp.rrzcp8.com>
2019-01-29 15:25:01 +0800

    [test:修改sh调用规则]

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ];then
   echo "Usage : $0 [PROJECT_NAME]" >&2
   exit 1
 fi
-# 清空目录 以下为前端资源 相关逻辑 拆分到前端项目的部署脚本中，暂时用此项目来测试
+# 清空目录 以下为前端资源 相关逻辑 拆分到前端项目的部署脚本中，暂时用此后端项目来测试
 # nginx目录 /usr/local/nginx/html/[PROJECT_NAME]/
 rm -rf /usr/local/nginx/html/$1/*
 # 拷贝静态资源
